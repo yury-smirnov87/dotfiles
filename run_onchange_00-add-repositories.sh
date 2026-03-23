efee65775552bb3f951fc0c81f1f3fbe662f9fd1
Yury Smirnov <yury@dell5560.local>
2026-03-23 14:56:12 -0400

    Initial commit of dotfiles for debian trixie

---
+++ run_onchange_00-add-repositories.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Configuring additional repositories"
+
+echo "Adding XanMod repository"
+wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -vo /etc/apt/keyrings/xanmod-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org $(lsb_release -sc) main non-free" | sudo tee /etc/apt/sources.list.d/xanmod-release.list    
+
+echo "Adding Dropbox repository"
+wget -qO - https://linux.dropbox.com/fedora/rpm-public-key.asc | tee /etc/apt/keyrings/dropbox.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/dropbox.gpg] http://linux.dropbox.com/debian trixie main" | tee /etc/apt/sources.list.d/dropbox.list
+
+sudo apt-get update
+
+echo "Finished configuring additional repositories"
---
+++ run_onchange_30-install-common-packages.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Installing general packages, fonts, themes, cursors, etc"
+
+export DEBIAN_FRONTEND=noninteractive
+
+PACKAGES=(
+    arc-theme
+    papirus-icon-theme
+    bibata-cursor-theme
+    fonts-jetbrains-mono
+    fonts-firacode
+    geany
+    curl
+    git
+    zip
+    unzip
+    flatpak
+    transmission
+)
+sudo apt-get install -y --no-install-recommends "${PACKAGES[@]}"
+
+echo "Updating fonts cache"
+
+fc-cache -fv
+
+echo "Applying Cinnamon settings..."
+
+gsettings set org.cinnamon.theme name "Arc"
+gsettings set org.cinnamon.desktop.interface gtk-theme "Arc"
+gsettings set org.cinnamon.desktop.wm.preferences theme "Arc"
+gsettings set org.cinnamon.desktop.interface icon-theme "Papirus"
+gsettings set org.cinnamon.desktop.interface cursor-theme "Bibata-Modern-Ice"
+gsettings set org.gnome.desktop.interface monospace-font-name "JetBrains Mono 11"
+
+echo "Finished Installing general packages, fonts, themes, cursors, etc."
