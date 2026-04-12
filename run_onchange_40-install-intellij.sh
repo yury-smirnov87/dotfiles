c6f7b9c587d3bf7ccdec26c23cdfba92fe2cd2cc
Yury Smirnov <yury.smirnov87@gmail.com>
2026-04-12 19:12:59 -0400

    Intellij Idea added

--- run_onchange_40-install-intellij.sh
+++ run_onchange_40-install-intellij.sh
@@ -11,12 +11,16 @@ INSTALL_DIR="/opt/$APP_NAME"
 ARCHIVE_URL="https://download.jetbrains.com/product?code=IU&latest&distribution=linux"
 ARCHIVE_OUT_FILE="$HOME/$APP_NAME.tar.gz"
 
-sudo mkdir -p "$INSTALL_DIR"
-
 curl -s -L -o "$ARCHIVE_OUT_FILE" "$ARCHIVE_URL"
+
+sudo mkdir -p "$INSTALL_DIR"
 
 sudo tar -xzf "$ARCHIVE_OUT_FILE" -C "$INSTALL_DIR" --strip-components=1
 
+sudo chown -R $USER:$USER "$INSTALL_DIR"
+
 sudo ln -s "$INSTALL_DIR/bin/idea" "/usr/local/bin/idea"
+
+rm -f "$ARCHIVE_OUT_FILE"
 
 echo "Finished installing IntelliJ Idea"
