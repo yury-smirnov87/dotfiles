13a0a1f75f0bb0ba993f4f308e9e1e4d628b646c
Yury Smirnov <yury.smirnov87@gmail.com>
2026-04-12 19:56:18 -0400

    Textadept install script improved

--- run_onchange_40-install-textadept.sh
+++ run_onchange_40-install-textadept.sh
@@ -7,13 +7,20 @@ export DEBIAN_FRONTEND=noninteractive
 echo "Installing Textadept"
 
 APP_NAME="textadept"
-INSTALL_DIR="/opt"
+INSTALL_DIR="/opt/$APP_NAME"
 ARCHIVE_URL="https://github.com/orbitalquark/textadept/releases/download/textadept_12.9/textadept_12.9.linux.tgz"
 ARCHIVE_OUT_FILE="$HOME/$APP_NAME.tgz"
 
 curl -s -L -o "$ARCHIVE_OUT_FILE" "$ARCHIVE_URL"
+
+sudo mkdir -p "$INSTALL_DIR"
+
 sudo tar -xf "$ARCHIVE_OUT_FILE" -C "$INSTALL_DIR"
+
+sudo chown -R $USER:$USER "$INSTALL_DIR"
+
 sudo ln -s "$INSTALL_DIR/$APP_NAME/textadept-gtk" /usr/local/bin/textadept-gtk
+
 rm -f $ARCHIVE_OUT_FILE
 
 echo "Finished installing Textadept"
